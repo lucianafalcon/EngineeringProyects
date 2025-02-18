8ac624162718bd1da8634b6321063886be29315e
lucianafalcon <lfalcon@fi.uba.ar>
2025-02-17 21:08:57 -0300

    commit

--- app.js
+++
@@ -1,8 +0,0 @@
-// Obtener el botón hamburguesa y los enlaces del menú
-const menuHamburguesa = document.querySelector('.menu__hamburguer');
-const menuLinks = document.querySelector('.menu__links');
-
-// Cuando se haga clic en el botón hamburguesa, alternar la visibilidad del menú
-menuHamburguesa.addEventListener('click', function() {
-    menuLinks.classList.toggle('show');
-});
